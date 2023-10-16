6745bac4de35c3cacb72df11c7dae6dfa20c8d15
Wayne <fssher107@gmail.com>
2023-10-16 21:28:21 +0800

    feat: run function on server start

---
+++ src/instrumentation.ts
@@ -0,0 +1,9 @@
+// Place any operations that needs to be ran once sever start inside function register()
+// NextJS doc: We recommend importing files with side effects using import from within your register function instead
+// example:
+// export async function register() {
+//     await import('package-with-side-effect')
+//   }
+export async function register() {
+  // import any modules here, not outside this function
+}
